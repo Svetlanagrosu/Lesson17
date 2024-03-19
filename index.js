6540d2ea3ba5e5996b86e4bc04320eaac8083aca
Svetlanagrosu <grosu.svetlana.miha@gmail.com>
2024-03-19 07:00:20 +0100

    secont commit

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-//EX1/1
-//const arr = [1,2,3,4,5,6]
-//console.log(arr)
 
-//EX1/2
-const arr = [1,2,3,4,5,6]
-console.log(arr.filter((item) => {
-    return item * 2;
-}))
-console.log(filteredArr)
+const closure = () => {
+    const arr = [1,2,3,4,5,]
+    function showArr() {
+        return arr;
+    }
+
+    function showEvenNumber() {
+        return arr.filter((item) => item%2 === 0);
+    }
+
+    function showOddNumber() {
+        return arr.filter((item) => item%2 !==0);
+    }
+
+    function showSum() {
+        let result = 0;
+        arr.forEach((item) => result+=item)
+        return result;
+    }
+    function addNumber(num) {
+        if(typeof num !== number && num.isNaN()) return;
+        arr.push(num);
+    }
+    return {showArr, showEvenNumber, showOddNumber, showSum, addNumber}; 
+
+}
+
+const { showArr, showEvenNumber, showOddNumber, showSum, addNumber} = closure ()
+
+console.log(showArr())
+console.log(showEvenNumber())
+console.log(showOddNumber())
+console.log(showSum())
+addNumber(showarr().length + 1)
+console.log(showArr())
 
 
-
-
-// EX 1/5
-//const arr = [1,2,3,4,5,6]
-//const returnValue = arr.push(7)
-//console.log(returnValue)
 
 
 
