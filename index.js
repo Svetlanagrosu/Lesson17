e86c1f1d120f0043ab3844de07a2936cfa050c17
Svetlanagrosu <grosu.svetlana.miha@gmail.com>
2024-03-19 06:53:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+//EX1/1
+//const arr = [1,2,3,4,5,6]
+//console.log(arr)
+
+//EX1/2
+const arr = [1,2,3,4,5,6]
+console.log(arr.filter((item) => {
+    return item * 2;
+}))
+console.log(filteredArr)
+
+
+
+
+// EX 1/5
+//const arr = [1,2,3,4,5,6]
+//const returnValue = arr.push(7)
+//console.log(returnValue)
+
+
+
+
+
